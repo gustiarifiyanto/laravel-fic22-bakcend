074d0705197148c948d81e5b9d9016d6ca6e90bb
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-02-02 11:33:00 +0700

    Melakukan penambahan CreatePaymentUrlService file dan mengatur flow dari order ticket pada OrderContorller file

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\models\Order;
+use App\Models\Sku;
+use App\Models\Ticket;
+use App\Models\OrderTicket;
 
 class OrderController extends Controller
 {
@@ -14,24 +17,53 @@ class OrderController extends Controller
         //validate the request
         $request->validate([
             "event-id"=> "required|exists:events,id",
-            "sku_id"=> "required|exists:skus,id",
+            "order_details" => "required|array",
+            "order_details.*.sku_id"=> "required|exists::skus,id",
             "quantity"=> "required|integer|min:1",
-            "customer_name"=> "required|string",
-            "customer_email"=> "required|email",
-            "customer_phone"=> "required|string",
-            "customer_address"=> "required|string",
+            "event_date"=> "required",
         ]);
+
+        //$event = Event::find($request->event_id);
+
+        $total = 0;
+        foreach ($request->order_details as $orderDetail) {
+            $sku =Sku::find($orderDetail['sku_id']);
+            $qty = $orderDetail['qty'];
+            $total += $sku->price * $qty;
+        }
 
         //create order
         $order = Order::create([
             "event_id"=> $request->event_id,
+            "event_date"=> $request->event_date,
             "sku_id"=> $request->sku_id,
-            "quanityt"=> $request->quantity,
-            "customer_name"=> $request->customer_name,
-            "customer_email"=> $request->customer_email,
-            "customer_phone"=> $request->customer_phone,
-            "customer_address"=> $request->customer_address,
+            "quantity"=> $request->quantity,
+            "total"=> $total,
         ]);
+
+        
+        foreach ($request->order_details as $orderDetail) {
+            $sku =Sku::find($orderDetail['sku_id']);
+            $qty = $orderDetail['qty'];
+            
+            for( $i = 0; $i < $qty; $i++ ) {
+                //ticket by sku and available
+                $ticket = Ticket::where('sku_id', $sku->id)
+                ->where('status', 'available')
+                ->first();
+                //insert order ticket
+                OrderTicket::create([
+                    'order_id'=> $order->id,
+                    'ticket'=> $ticket->id,
+                ]);
+                //ticket status to sold
+                $ticket->update([
+                    'status'=> 'booked',
+                ]);
+            }
+        }
+
+        
 
         //return response
         return response()->json([
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'quantity',
+        'total',
+        'status',
+        'event_date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function skus()
+    {
+        return $this->belongsTo(Skus::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
